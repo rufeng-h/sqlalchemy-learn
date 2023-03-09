9d5ce91a9e67e729572543e529f8fad448cfc573
rufeng-h <chunfengh163@163.com>
2023-03-09 17:09:00 +0800

    commit

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ from models import Post
 
 def load_db_auth():
     pwd = os.path.dirname(__file__)
-    auth = json.loads(Path(os.path.join(pwd, 'db.json.local')).read_text(encoding='utf-8'))
-    return auth['username'], auth['password']
+    return json.loads(Path(os.path.join(pwd, 'db.json.local')).read_text(encoding='utf-8'))
 
 
-username, password = load_db_auth()
+db_auth = load_db_auth()
 
-engine = create_engine("mysql+pymysql://%s:%s@81.71.161.25:3306/halodb" % (username, quote_plus(password)), echo=True)
+engine = create_engine("mysql+pymysql://%s:%s@%s:%s/%s" % (
+    db_auth['username'], quote_plus(db_auth['password']), db_auth['host'], db_auth['port'], db_auth['database']),
+                       echo=True)
 with Session(engine) as session:
     stmt = select(Post).where(Post.create_time > datetime.datetime(2023, 1, 1))
     scalars = session.scalars(stmt)
