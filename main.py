c626f02bf8c8fa049bdc237f3ffed2394efbed5f
rufeng-h <chunfengh163@163.com>
2023-03-09 19:03:36 +0800

    succ

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
-import json
+import configparser
 import os
-from pathlib import Path
 from urllib.parse import quote_plus
 
+import requests
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
 
-from models import Post, Option
+from models import Post
 
 
 def load_db_auth():
+    parser = configparser.ConfigParser()
     pwd = os.path.dirname(__file__)
-    return json.loads(Path(os.path.join(pwd, 'db.json.local')).read_text(encoding='utf-8'))
+    parser.read(os.path.join(pwd, 'db.conf'))
+    return parser
 
 
-db_auth = load_db_auth()
+db_auth = load_db_auth()['auth']
 
 engine = create_engine("mysql+pymysql://%s:%s@%s:%s/%s" % (
     db_auth['username'], quote_plus(db_auth['password']), db_auth['host'], db_auth['port'], db_auth['database']),
                        echo=True)
-with Session(engine) as session:
-    stmt = select(Post.id).order_by(Post.create_time.desc())
-    scalars = session.scalars(stmt)
-    ids = scalars.fetchall()
-    stmt = select(Option.option_value).where(Option.option_key == "post_permalink_type")
-    post_link_type = session.scalar(stmt)
-print(ids)
-print(post_link_type)
+
+if __name__ == '__main__':
+    with Session(engine) as session:
+        stmt = select(Post.id).order_by(Post.create_time.desc())
+        scalars = session.scalars(stmt)
+        ids = scalars.fetchall()
+
+blog_url_body = '\r\n'.join([f"http://windcf.com/archives/{blog_id}" for blog_id in ids])
+
+response = requests.post('http://data.zz.baidu.com/urls?site=windcf.com&token=X95hGKj0yvEILNyl', data=blog_url_body)
