8188566d65bcf6be98c6973a9958ac1395e63caa
rufeng-h <chunfengh163@163.com>
2023-03-10 17:46:09 +0800

    dsaf

--- main.py
+++ main.py
@@ -16,18 +16,22 @@ def load_db_auth():
     return parser
 
 
-db_auth = load_db_auth()['auth']
+def get_engine():
+    db_auth = load_db_auth()['auth']
 
-engine = create_engine("mysql+pymysql://%s:%s@%s:%s/%s" % (
-    db_auth['username'], quote_plus(db_auth['password']), db_auth['host'], db_auth['port'], db_auth['database']),
+    en = create_engine("mysql+pymysql://%s:%s@%s:%s/%s" % (
+        db_auth['username'], quote_plus(db_auth['password']), db_auth['host'], db_auth['port'], db_auth['database']),
                        echo=True)
+    return en
+
 
 if __name__ == '__main__':
+    engine = get_engine()
     with Session(engine) as session:
         stmt = select(Post.id).order_by(Post.create_time.desc())
         scalars = session.scalars(stmt)
         ids = scalars.fetchall()
 
-blog_url_body = '\r\n'.join([f"http://windcf.com/archives/{blog_id}" for blog_id in ids])
+    blog_url_body = '\r\n'.join([f"http://windcf.com/archives/{blog_id}" for blog_id in ids])
 
-response = requests.post('http://data.zz.baidu.com/urls?site=windcf.com&token=X95hGKj0yvEILNyl', data=blog_url_body)
+    response = requests.post('http://data.zz.baidu.com/urls?site=windcf.com&token=X95hGKj0yvEILNyl', data=blog_url_body)
--- tests.py
+++ tests.py
@@ -1,11 +1,16 @@
 import sys
 import unittest
+
 from loguru import logger
+from sqlalchemy.orm import Session
+
+from main import get_engine
+from models import Post
+
 
 def fil(record):
     print(record)
     return True
-
 
 
 class Test(unittest.TestCase):
@@ -16,3 +21,9 @@ class Test(unittest.TestCase):
                    filter=fil)
         logger.debug("dafddsaf")
         logger.info("dafafasdfadsf")
+
+    def test_read(self):
+        engine = get_engine()
+        with Session(engine) as session:
+            post = session.get(Post, 5)
+            print(post.format_content)
